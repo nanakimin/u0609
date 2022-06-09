dc8e5ea1e75e2d55135e3d3a745f55c49aee4acb
A601-25 <oudontabetai0101@gmail.com>
2022-06-09 15:01:38 +0900

    Initial commit

---
+++ Assets/hensu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hensu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int a=10;
+        float fw=3.14f;
+        string str="‚±‚ñ‚É‚¿‚Í";
+        bool b1=true;
+
+        Debug.Log(a);
+        Debug.Log(fw);
+        Debug.Log(str);
+        Debug.Log(b1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
